3f62a3e9ad2974bb8a614089531d12bf07f914e0
Matt Carter <m@ttcarter.com>
2015-12-25 15:36:01 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+var async = require('async-chainable');
+var asyncExec = require('async-chainable-exec');
+var childProcess = require('child_process');
+var electron = require('electron');
+
+var win;
+
+// Global functions {{{
+function restyleWindow(finish) {
+	console.log('PID', process.pid);
+	async()
+		.use(asyncExec)
+		.execDefaults({
+			log: function(cmd) { console.log('[RUN]', cmd.cmd + ' ' + cmd.params.join(' ')) },
+			out: function(line) { console.log('[GOT]', line) },
+		})
+		.exec([
+			'wmctrl', 
+			'-F',
+			'-r',
+			'Conker',
+			'-b',
+			'add,below',
+			'-vvv',
+		])
+		.exec([
+			'wmctrl', 
+			'-F',
+			'-r',
+			'Conker',
+			'-b',
+			'add,sticky',
+			'-vvv',
+		])
+		.end(finish);
+}
+// }}}
+
+var app = electron.app
+	.on('window-all-closed', function() {
+		if (process.platform != 'darwin') app.quit(); // Kill everything if we're on Darwin
+	})
+	.on('ready', function() {
+		// Create the browser window.
+		win = new electron.BrowserWindow({
+			width: 800,
+			height: 600,
+			frame: false,
+			resizable: false,
+			skipTaskbar: true,
+			title: 'Conker',
+			type: 'desktop',
+			show: false,
+		});
+
+		win.loadURL('file://' + __dirname + '/index.html');
+		win.showInactive();
+
+		win.webContents.on('dom-ready', function() {
+			console.log('LOADED!');
+			restyleWindow();
+		});
+
+		win
+			.on('page-title-updated', function(e) {
+				// Prevent title changes so we can always find the window
+				e.preventDefault();
+			})
+			.on('closed', function() {
+				win = null; // Remove reference and probably terminate the program
+			});
+
+		console.log(win);
+	});
