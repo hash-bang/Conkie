d41144889d2bb86115f042d6ff25a59717970c3f
Matt Carter <m@ttcarter.com>
2015-12-25 17:06:43 +1000

    Added prototype battery functionality

--- themes/mc-sidebar/javascript.js
+++ themes/mc-sidebar/javascript.js
@@ -1,7 +1,33 @@
 var app = angular.module('app', []);
 
 app.controller('conkerController', function($scope) {
+	// .battery {{{
 	$scope.battery = {
-		power: 100,
+		charging: false,
+		levelPercent: 100,
+		chargingTime: undefined,
+		dischargingTime: undefined,
 	};
+
+	navigator.getBattery().then(function(battery) {
+		var batteryUpdate = function() {
+			console.log('BATTERY UPDATE', battery);
+			$scope.$apply(function() {
+				$scope.battery.charging = battery.charging;
+				$scope.battery.chargingTime = battery.chargingTime;
+				$scope.battery.dischargingTime = battery.dischargingTime;
+				$scope.battery.levelPercent = Math.ceil(battery.level * 100);
+			});
+		};
+
+		battery.onchargingchange
+		= battery.onchargingtimechange
+		= battery.ondischargingtimechange
+		= battery.onlevelchange
+		= batteryUpdate;
+
+		batteryUpdate();
+	});
+	// }}}
+
 });
